16c0cdd0c4df581bfee125473aa6dbc3e6506a0b
Nico Lehmann <nico.lehmannm@gmail.com>
2023-11-17 21:51:45 -0800

    Add basic textmate grammar

---
+++ tools/ocimatic-testplan/syntaxes/asdf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Bash script to greet the user
+
+# Prompt the user for their name
+echo asdf hola 1
+read -p "Enter your name: " userName
+
+# Greet the user
+echo "Hello, $userName! Welcome to the Bash scripting world."
+
