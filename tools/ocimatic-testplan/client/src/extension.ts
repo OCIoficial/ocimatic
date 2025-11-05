82b73965943a228e268cc56b3e9d75d3ee6fd077
Nico Lehmann <nico.lehmannm@gmail.com>
2025-11-04 22:22:46 -0300

    Check if ocimatic is in the PATH

--- tools/ocimatic-testplan/client/src/extension.ts
+++ tools/ocimatic-testplan/client/src/extension.ts
@@ -1,25 +1,35 @@
 import * as vscode from "vscode";
 import { LanguageClient, LanguageClientOptions, ServerOptions } from "vscode-languageclient/node";
+import { exec } from "child_process";
 
 export async function activate(_context: vscode.ExtensionContext) {
-  // Server options: run 'ocimatic lsp'
-  const serverOptions: ServerOptions = {
-    command: "ocimatic",
-    args: ["lsp"],
-  };
+  exec("ocimatic version", (err) => {
+    if (err) {
+      vscode.window.showWarningMessage(
+        "Ocimatic command not found. Please install 'ocimatic' and ensure it is in your PATH."
+      );
+      return;
+    }
 
-  // Client options
-  const clientOptions: LanguageClientOptions = {
-    documentSelector: [{ scheme: "file", language: "ocimatic-testplan" }],
-  };
+    // Server options: run 'ocimatic lsp'
+    const serverOptions: ServerOptions = {
+      command: "ocimatic",
+      args: ["lsp"],
+    };
 
-  // Create and start the language client
-  const client = new LanguageClient(
-    "ocimatic-language-server",
-    "Ocimatic",
-    serverOptions,
-    clientOptions
-  );
+    // Client options
+    const clientOptions: LanguageClientOptions = {
+      documentSelector: [{ scheme: "file", language: "ocimatic-testplan" }],
+    };
 
-  client.start();
+    // Create and start the language client
+    const client = new LanguageClient(
+      "ocimatic-language-server",
+      "Ocimatic",
+      serverOptions,
+      clientOptions
+    );
+
+    client.start();
+  });
 }
